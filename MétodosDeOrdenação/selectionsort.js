14ba3df283d3446238f49b6892f3786db3b45180
Ana Júlia Andrade <131200086+anajuliaandrade@users.noreply.github.com>
2024-02-26 14:12:44 -0300

    Add files via upload

---
+++ MétodosDeOrdenação/selectionsort.js
@@ -0,0 +1,18 @@
+function selectionsort(){
+    let vetor =[5,55,80,1,7]
+    alert(`Vetor desordenado: ${vetor}`)
+    let menor
+    for (let i=0; i<(vetor.length - 1); i++) {//define a quant vezes q vai percorrer o vetor
+        menor=i
+        for (let j =i+1; j<vetor.length; j++){//percorre vetor
+            if(vetor[j]<vetor[menor]){
+                menor=j
+            }
+        }
+        if(i != menor){
+            [vetor[i], vetor [menor]] = [vetor[menor], vetor[i]]
+        }
+    }
+    alert(`Vetor ordenado: ${vetor}`)
+}
+selectionsort()
